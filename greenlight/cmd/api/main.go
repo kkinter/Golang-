88a52f4acd14220b8766b1a0301b5f72d2fdbf88
kkinter <ifol1129@gmail.com>
2023-05-12 08:35:36 +0900

    DB setup and config

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -1,52 +1,67 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
-// 애플리케이션 버전 번호가 포함된 문자열을 선언합니다.
-// 이 책의 뒷부분에서는 빌드 시점에 자동으로 생성하지만
-// 지금은 버전 번호를 하드코딩된 전역 상수로 저장하겠습니다.
 const version = "1.0.0"
 
-// 애플리케이션의 모든 구성 설정을 저장할 구성 구조체를 정의합니다.
-// 현재로서는 서버가 수신 대기할 네트워크 포트와 애플리케이션의 현재 운영 환경 이름(개발 및 스테이징)만 구성 설정에 포함됩니다.
-// 애플리케이션의 현재 운영 환경 이름(개발, 스테이징, 프로덕션 등)입니다.
-// 애플리케이션이 시작될 때 명령줄 플래그에서 이러한 구성 설정을 읽어들일 것입니다.
+// 연결 풀에 대한 구성 설정을 저장하기 위해 maxOpenConns,
+// maxIdleConns 및 maxIdleTime 필드를 추가합니다.
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
-// HTTP 핸들러, 헬퍼, 미들웨어에 대한 종속성을 담을 애플리케이션 구조체를 정의합니다.
-// 현재는 구성 구조체와 로거의 복사본만 포함되어 있지만
-// 빌드가 진행됨에 따라 더 많은 것을 포함하도록 확장할 것입니다.
 type application struct {
 	config config
 	logger *log.Logger
 }
 
 func main() {
-	// config 구조체의 인스턴스를 선언합니다.
 	var cfg config
 
-	// port  및 env 명령줄 플래그 값을 config 구조체에 읽어들입니다.
-	//해당 플래그가 제공되지 않으면 기본적으로 포트 번호 4000과
-	// environment "development"을 사용합니다.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production")
+	// db-dsn 명령줄 플래그의 DSN 값을 config 구조체로 읽습니다.
+	// 플래그가 제공되지 않으면 기본적으로 development DSN을 사용합니다.
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	// 명령줄 플래그에서 연결 풀 설정을 config 구조체로 읽습니다.
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
-	// 현재 날짜와 시간이 접두사로 붙은 메시지를 표준 출력 스트림에 기록하는
-	// 새 로거를 초기화합니다.
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	// config 구조체와 logger 포함하는 애플리케이션 구조체의 인스턴스를 선언합니다.
+	// config 구조체를 전달하면서 openDB() 도우미 함수(아래 참조)를 호출하여 연결 풀을 생성합니다.
+	// 오류가 반환되면 오류를 기록하고 즉시 애플리케이션을 종료합니다.
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Printf("데이터베이스 연결 풀 설정됨")
+
+	defer db.Close()
+
 	app := &application{
 		config: cfg,
 		logger: logger,
@@ -61,6 +76,41 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// 풀에서 열려 있는(사용 중 + 유휴) 연결의 최대 수를 설정합니다.
+	//  0보다 작은 값을 전달하면 제한이 없음을 의미
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+	// 풀의 최대 유휴 연결 수를 설정합니다.
+	// 0보다 작은 값을 전달하면 제한이 없다는 의미
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	// time.ParseDuration() 함수를 사용하여 idle timeout duration
+	// 문자열을 time.Duration 유형으로 변환합니다.
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	// maximum idle timeout 설정.
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
