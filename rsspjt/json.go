84a817f59c9539159ac657420b24c07e9a17c2bc
kkinter <ifol1129@gmail.com>
2023-05-17 06:37:56 +0900

    config pjt

---
+++ rsspjt/json.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("Failed to marshal JSON response: %v", payload)
+		w.WriteHeader(500) // internal server err
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
