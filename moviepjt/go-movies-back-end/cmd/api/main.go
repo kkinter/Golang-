94fedf55c0fc3eadd7e0e238defdc25079c35666
kkinter <ifol1129@gmail.com>
2023-05-20 11:25:10 +0900

    Config DB

--- moviepjt/go-movies-back-end/cmd/api/main.go
+++ moviepjt/go-movies-back-end/cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"backend/internal/repository"
+	"backend/internal/repository/dbrepo"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +12,9 @@ import (
 const port = 8000
 
 type application struct {
+	DSN    string
 	Domain string
+	DB     repository.DatabaseRepo
 }
 
 // entry point for app
@@ -18,15 +23,26 @@ func main() {
 	var app application
 
 	// read from command line e.g) flag
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
 
 	// connect to the db
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
+
+	// defer conn.Close()
 
 	app.Domain = "example.com"
 
 	// start a web server
 	log.Println("Starting app on port:", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
