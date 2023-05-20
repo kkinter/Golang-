366614e9c9d8df9a5eca5ed2cdbb1b55779aad98
kkinter <ifol1129@gmail.com>
2023-05-20 13:54:35 +0900

    Feat: jwt

--- moviepjt/go-movies-back-end/cmd/api/main.go
+++ moviepjt/go-movies-back-end/cmd/api/main.go
@@ -7,14 +7,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = 8000
 
 type application struct {
-	DSN    string
-	Domain string
-	DB     repository.DatabaseRepo
+	DSN          string
+	Domain       string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 // entry point for app
@@ -24,6 +30,12 @@ func main() {
 
 	// read from command line e.g) flag
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "localhost", "domain")
+
 	flag.Parse()
 
 	// connect to the db
@@ -35,9 +47,16 @@ func main() {
 	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
 	defer app.DB.Connection().Close()
 
-	// defer conn.Close()
-
-	app.Domain = "example.com"
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
 
 	// start a web server
 	log.Println("Starting app on port:", port)
--- moviepjt/go-movies-back-end/internal/repository/repository.go
+++ moviepjt/go-movies-back-end/internal/repository/repository.go
@@ -8,4 +8,5 @@ import (
 type DatabaseRepo interface {
 	Connection() *sql.DB
 	AllMovies() ([]*models.Movie, error)
+	GetUserNyEmail(email string) (*models.User, error)
 }
