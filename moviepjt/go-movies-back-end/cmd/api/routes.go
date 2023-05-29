04680733e709f68c79ab659edebad2d5678e5dac
kkinter <ifol1129@gmail.com>
2023-05-29 20:50:38 +0900

    Feat: middleware

--- moviepjt/go-movies-back-end/cmd/api/routes.go
+++ moviepjt/go-movies-back-end/cmd/api/routes.go
@@ -21,5 +21,10 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/movies", app.AllMovies)
 
+	mux.Route("/admin", func(r chi.Router) {
+		r.Use(app.authRequired)
+		r.Get("/movies", app.MovieCatalog)
+	})
+
 	return mux
 }
