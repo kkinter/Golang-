425381d41c0b26feb4e58efecf287114a0d90f5d
kkinter <ifol1129@gmail.com>
2023-08-16 05:35:19 +0900

    Feat: server package

--- newspjt/go.mod
+++ newspjt/go.mod
@@ -1,3 +1,8 @@
 module newspjt
 
 go 1.20
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/matryer/is v1.4.1
+)
--- newspjt/server/server.go
+++ newspjt/server/server.go
@@ -1,1 +1,64 @@
 package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	address string
+	mux     chi.Router
+	server  *http.Server
+}
+
+type Options struct {
+	Host string
+	Port int
+}
+
+func New(opts Options) *Server {
+	address := net.JoinHostPort(opts.Host, strconv.Itoa(opts.Port))
+	mux := chi.NewMux()
+	return &Server{
+		address: address,
+		mux:     mux,
+		server: &http.Server{
+			Addr:              address,
+			Handler:           mux,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: 5 * time.Second,
+			WriteTimeout:      5 * time.Second,
+			IdleTimeout:       5 * time.Second,
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	s.setupRoutes()
+
+	fmt.Println("시작 중", s.address)
+	if err := s.server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("서버 시작 중에 %w 에러가 발생하였습니다.", err)
+	}
+	return nil
+}
+
+func (s *Server) Stop() error {
+	fmt.Println("중지 중")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.server.Shutdown(ctx); err != nil {
+		return fmt.Errorf("서버 중지 중에 %w 에러가 발생하였습니다.", err)
+	}
+
+	return nil
+}
