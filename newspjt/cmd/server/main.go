cf9ec67b01e124954d1558b1f0571573aec772c6
kkinter <ifol1129@gmail.com>
2023-08-18 00:54:55 +0900

    Feat: main func

--- newspjt/cmd/server/main.go
+++ newspjt/cmd/server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"newspjt/server"
 	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -22,6 +28,40 @@ func start() int {
 	defer func() {
 		_ = log.Sync()
 	}()
+
+	host := getStringOrDefault("HOST", "localhost")
+	port := getIntOrDefault("PORT", 8080)
+
+	s := server.New(server.Options{
+		Host: host,
+		Port: port,
+	})
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := s.Start(); err != nil {
+			log.Info("Error starting server", zap.Error(err))
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+
+	eg.Go(func() error {
+		if err := s.Stop(); err != nil {
+			log.Info("Error stopping server", zap.Error(err))
+			return err
+		}
+		return nil
+	})
+
+	if err := eg.Wait(); err != nil {
+		return 1
+	}
 
 	return 0
 }
@@ -43,4 +83,16 @@ func getStringOrDefault(name, defaultV string) string {
 		return defaultV
 	}
 	return v
+}
+
+func getIntOrDefault(name string, defaultV int) int {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	vAsInt, err := strconv.Atoi(v)
+	if err != nil {
+		return defaultV
+	}
+	return vAsInt
 }
