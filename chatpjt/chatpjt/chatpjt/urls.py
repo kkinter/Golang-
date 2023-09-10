23c78cbbaecb23d9e3907d4572f3504c383a456d
kkinter <ifol1129@gmail.com>
2023-09-10 10:09:04 +0900

    first commit

---
+++ chatpjt/chatpjt/chatpjt/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/schema/ui", SpectacularSwaggerView.as_view(), name="swagger_view"),
+]
