92b237a188cb789f86aecf07ef1cd8865002288b
kkinter <ifol1129@gmail.com>
2023-09-14 00:00:15 +0900

    first commit

---
+++ pycommerce/ecommerce/ecommerce/product/views.py
@@ -0,0 +1,45 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Brand, Category, Product
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    모든 카테고리 조회를 위한 Viewset
+    """
+
+    queryset = Category.objects.all()
+
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    모든 브랜드 조회를 위한 Viewset
+    """
+
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductViewSet(viewsets.ViewSet):
+    """
+    모든 상품 조회를 위한 Viewset
+    """
+
+    queryset = Product.objects.all()
+
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
