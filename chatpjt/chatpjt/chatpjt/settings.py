8ccc90d6a4382f8ce99491e900efcac97e1fc772
kkinter <ifol1129@gmail.com>
2023-09-18 07:24:01 +0900

    Feat: Model

--- chatpjt/chatpjt/chatpjt/settings.py
+++ chatpjt/chatpjt/chatpjt/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # ex
     "drf_spectacular",
+    "rest_framework",
     # in
     "server",
     "account",
@@ -112,6 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -124,7 +127,7 @@ AUTH_USER_MODEL = "account.Account"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthenticationn",
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
 
