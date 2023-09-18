5b369e7c6d495c9f00bb9c3bc40784b75c97a62d
kkinter <ifol1129@gmail.com>
2023-09-18 10:39:47 +0900

    Feat: App Layout

--- chatpjt/frontend/src/pages/Home.tsx
+++ chatpjt/frontend/src/pages/Home.tsx
@@ -1,12 +1,18 @@
 import { Box, CssBaseline } from "@mui/material";
 import PrimayAppBar from "./templates/PrimaryAppBar";
+import PrimaryDraw from "./templates/PrimaryDraw";
+import SecondaryDraw from "./templates/SecondaryDraw";
+import Main from "./templates/Main";
 
 const Home = () => {
 
     return (
         <Box sx={{ display: "flex"}}>
             <CssBaseline />
-            <PrimayAppBar />
+            <PrimayAppBar />     
+            <PrimaryDraw />
+            <SecondaryDraw />
+            <Main />
         </Box>
     );
 };
