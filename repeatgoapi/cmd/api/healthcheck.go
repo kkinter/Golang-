19fcdb849982e6ffa995b24fb07b14dbe0569c97
kkinter <ifol1129@gmail.com>
2023-10-03 02:20:35 +0900

    Feat: showMovieHandler

--- repeatgoapi/cmd/api/healthcheck.go
+++ repeatgoapi/cmd/api/healthcheck.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "서버에 문제가 발생하여 요청을 처리할 수 없습니다.", http.StatusInternalServerError)
+	}
 }
--- repeatgoapi/cmd/api/movies.go
+++ repeatgoapi/cmd/api/movies.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"wook.net/internal/data"
 )
 
 func (app *application) creteMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,4 +18,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	pathParams := chi.URLParam(r, "id")
 	fmt.Fprintf(w, "show movies: %s", pathParams)
 
+	movie := data.Movie{
+		ID:        pathParams,
+		CreatedAt: time.Now(),
+		Title:     "Avengers",
+		Runtime:   120,
+		Genres:    []string{"action", "war", "hero"},
+		Version:   1,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "서버에 문제가 발생하여 요청을 처리할 수 없습니다.", http.StatusInternalServerError)
+	}
 }
