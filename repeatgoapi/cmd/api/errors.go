e49515206c95891a76c80bebc23f55a72b2d0789
kkinter <ifol1129@gmail.com>
2023-10-03 10:32:29 +0900

    you're doing great

--- repeatgoapi/cmd/api/errors.go
+++ repeatgoapi/cmd/api/errors.go
@@ -2,16 +2,18 @@ package main
 
 import "net/http"
 
+type Env struct {
+	Error string `json:"error"`
+}
+
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Print(err)
 }
 
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
-	env := {"error": message}
-	// writeJSON() 헬퍼를 사용하여 응답을 작성합니다. 이 과정에서 오류가 반환되면
-	// 이를 기록하고 클라이언트에 500 내부 서버 오류 상태 코드가 포함된
-	// 빈 응답을 보내는 것으로 되돌아갑니다.
-	err := app.writeJSON(w, status, env, nil)
+	var env Env
+
+	err := app.writeJSON(w, status, env.Error, nil)
 	if err != nil {
 		app.logError(r, err)
 		w.WriteHeader(500)
